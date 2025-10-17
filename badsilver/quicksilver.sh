4dca5b1e18f50c00400eea107bef0662b7d84283
Moonsploit <155044100+Moonsploit@users.noreply.github.com>
2025-10-17 19:25:19 +0100

    Update banner and maintain script functionality

--- badsilver/quicksilver.sh
+++ badsilver/quicksilver.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 clear
 echo ""
-echo " ██████╗  █████╗ ██████╗ ███████╗██╗██     ██╗   ██╗███████╗██████╗ "
-echo " ██╔══██╗██╔══██╗██╔══██╗██╔════╝██║██║    ██║   ██║██╔════╝██╔══██╗"
-echo " ██████╔╝███████║██║  ██║███████╗██║██║    ██║   ██║█████╗  ██████╔╝"
-echo " ██╔══██╗██╔══██║██║  ██║╚════██║██║██║    ╚██╗ ██╔╝██╔══╝  ██╔══██╗"
-echo " ██████╔╝██║  ██║██████╔╝███████║██║███████╗╚████╔╝ ███████╗██║  ██║"
-echo " ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝╚══════╝ ╚═══╝  ╚══════╝╚═╝  ╚═╝"
-echo "              kv6 unenrollment for keyrolled devices"
+echo " ██████╗  █████╗ ██████╗ ███████╗██╗██╗    ██╗   ██╗███████╗██████╗ "
+echo " ██╔══██╗██╔══██╗██╔══██╗██╔════╝██║██║    ██║   ██║██╔════╝██╔══██╗"
+echo " ██████╔╝███████║██║  ██║███████╗██║██║    ██║   ██║█████╗  ██████╔╝"
+echo " ██╔══██╗██╔══██║██║  ██║╚════██║██║██║    ╚██╗ ██╔╝██╔══╝  ██╔══██╗"
+echo " ██████╔╝██║  ██║██████╔╝███████║██║███████╗╚████╔╝ ███████╗██║  ██║"
+echo " ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝╚══════╝ ╚═══╝  ╚══════╝╚═╝  ╚═╝"
+echo "               kv6 unenrollment for keyrolled devices"
 echo ""
 
 echo "1) Unenroll"
@@ -15,25 +15,25 @@ echo "2) Re-enroll"
 read -p "Choose option: " choice
 
 case $choice in
-    1)
-        echo "Generating re-enrollment key..."
-        if vpd -i RW_VPD -s re_enrollment_key="$(hexdump -e '1/1 "%02x"' -v -n 32 /dev/urandom)"; then
-            echo "Unenrollment complete!"
-        else
-            echo "Failed to set re-enrollment key"
-            exit 1
-        fi
-        ;;
-    2)
-        if vpd -i RW_VPD -d "re_enrollment_key"; then
-            echo "Re-enrollment complete!"
-        else
-            echo "Failed to remove re-enrollment key"
-            exit 1
-        fi
-        ;;
-    *)
-        echo "Invalid option"
-        exit 1
-        ;;
+    1)
+        echo "Generating re-enrollment key..."
+        if vpd -i RW_VPD -s re_enrollment_key="$(hexdump -e '1/1 "%02x"' -v -n 32 /dev/urandom)"; then
+            echo "Unenrollment complete!"
+        else
+            echo "Failed to set re-enrollment key"
+            exit 1
+        fi
+        ;;
+    2)
+        if vpd -i RW_VPD -d "re_enrollment_key"; then
+            echo "Re-enrollment complete!"
+        else
+            echo "Failed to remove re-enrollment key"
+            exit 1
+        fi
+        ;;
+    *)
+        echo "Invalid option"
+        exit 1
+        ;;
 esac
