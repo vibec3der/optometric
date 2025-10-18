6e8842593d7cbf4a43878fc2e2c0e3f8c729cd1e
Moonsploit <155044100+Moonsploit@users.noreply.github.com>
2025-10-18 15:22:05 +0100

    Update badsilver.sh

--- badsilver/badsilver.sh
+++ badsilver/badsilver.sh
@@ -14,43 +14,29 @@ while true; do
     echo ""
 
     echo "1) Unenroll"
-    echo "2) Reenroll" 
-    echo "3) ckaub"
-    echo "4) Shell"
-    echo "5) Reboot"
+    echo "2) Reenroll"
+    echo "3) Shell"
+    echo "4) Reboot"
     read -p "Choose option: " choice
 
     case $choice in
         1)
-            if vpd -i RW_VPD -s re_enrollment_key="$(hexdump -e '1/1 "%02x"' -v -n 32 /dev/urandom)" 2>/dev/null; then
-                echo "Unenrollment success!"
-                echo "Returning to menu in 3 seconds..."
-                sleep 3
-            else
-                echo "Error: failed to set re_enrollment_key"
-                echo "Returning to menu in 3 seconds..."
-                sleep 3
-            fi
+            vpd -i RW_VPD -s re_enrollment_key="$(hexdump -e '1/1 "%02x"' -v -n 32 /dev/urandom)"
+            echo "Unenrollment complete!"
+            echo "Returning to menu in 3 seconds..."
+            sleep 3
             ;;
         2)
-            if vpd -i RW_VPD -d "re_enrollment_key" 2>/dev/null; then
-                echo "Reenrollment success!"
-                echo "Returning to menu in 3 seconds..."
-                sleep 3
-            else
-                echo "Error: failed to delete re_enrollment_key"
-                echo "Returning to menu in 3 seconds..."
-                sleep 3
-            fi
+            vpd -i RW_VPD -d "re_enrollment_key"
+            echo "Reenrollment complete!"
+            echo "Returning to menu in 3 seconds..."
+            sleep 3
             ;;
         3)
-            sh /usb/usr/sbin/ckaub.sh
-            ;;
-        4)
             echo "type 'exit' to go back to main menu"
             /bin/sh
             ;;
-        5)
+        4)
             reboot -f
             ;;
         *)
